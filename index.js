bb52f4c5c248b15a16f319ce533008d31764cd19
sadiqss <sadiq.sau48@gmail.com>
2022-05-23 14:13:41 +0100

    mongodb connected with server

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a37vj.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const partCollection = client.db('saltburn-auto').collection('parts');
+
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = partCollection.find(query);
+            const parts = await cursor.toArray();
+            res.send(parts);
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from Saltburn Auto Solution!')
